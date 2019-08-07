84825277775b1c64049353892fbb825b1da7c36a
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2019-08-07 00:34:40 -0300

    Criado e codificado o model Dev.

---
+++ src/app/models/Dev.js
@@ -0,0 +1,24 @@
+import { Schema, model } from 'mongoose';
+
+const DevSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: String,
+      required: true,
+    },
+    bio: String,
+    avatar: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+export default model('Dev', DevSchema);
